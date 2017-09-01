a571ff8d43c5322b0a1121416ddc71d77f9858e2
DenisWP <denis.dabliu@gmail.com>
2017-09-01 15:31:46 -0300

    Início do Desenvolvimento

---
+++ app/src/main/java/com/example/farid/denoappcel/actTelaInicial.java
@@ -0,0 +1,13 @@
+package com.example.farid.denoappcel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class actTelaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_tela_inicial);
+    }
+}
